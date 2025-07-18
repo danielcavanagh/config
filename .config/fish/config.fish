7964d98f5958eb6c9cfcd63a07df39087a04cd81
Daniel Cavanagh <danielcavanagh85@gmail.com>
2025-07-18 17:41:27 +1000

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,14 @@
-brew shellenv | source
+if type -q brew
+  brew shellenv | source
 
-$HOMEBREW_PREFIX/opt/mise/bin/mise activate fish | source
+  $HOMEBREW_PREFIX/opt/mise/bin/mise activate fish | source
+else
+  if type -q nvm; nvm use -s latest; end
+  if [ -x ~/.local/bin/mise ]; ~/.local/bin/mise activate fish | source; end
+end
 
 if status is-interactive
-  #cd ~/code
+  if [ (pwd) = /mnt/c/Users/dpcavan ]; cd ~; end
 
   if [ -z "$TMUX" ]
     tmux has > /dev/null
